4a701579ae0be31131bfb2689d158179cba51565
Renan <renan.lowe@hotmail.com>
2023-04-24 08:35:26 -0300

    add new tests

--- intermediate/test.hs
+++ intermediate/test.hs
@@ -8,3 +8,14 @@ tupla2 = (("Hello", 4), True)
 
 elemento :: Int
 elemento = snd (fst tupla2)
+
+-- ================================
+
+tupla3 :: (Int, String)
+tupla3 = (4, "ola")
+
+xx :: Int
+xx = fst tupla3
+
+yy :: String
+yy = snd tupla3
