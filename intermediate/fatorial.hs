ba5a5f61187dbcb770e4841d604d084cb15d740e
Renan <renan.lowe@hotmail.com>
2023-04-24 17:30:23 -0300

    calcular potencia de 2

--- intermediate/fatorial.hs
+++ intermediate/fatorial.hs
@@ -2,6 +2,13 @@ fatorial :: Integer -> Integer
 fatorial 0 = 1
 fatorial n = n * fatorial (n-1)
 
+--funcao que calcula o tamanho de uma lista
 length' :: [a] -> Integer
 length' [] = 0
 length' (_:xs) = 1 + length' xs
+
+--funcao que calcula a potencia de dois
+potDois :: Integer -> Integer
+potDois n 
+    | n == 0 = 1
+    | otherwise = 2 * potDois (n-1)
