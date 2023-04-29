7ad65386bbb9168f1f45a3e25d305678ce1d95e4
Renan <renan.lowe@hotmail.com>
2023-04-29 11:40:38 -0300

    lambda expressions

---
+++ intermediate/lambda.hs
@@ -0,0 +1,17 @@
+-- Funcao lambda que retorna o triplo de um numero
+x :: Int -> Int
+x = \x -> x * 3
+
+-- Funcao lamba que verifica se um numero é par
+n :: Int -> Bool
+n = \n -> if mod n 2 == 0 
+                then True 
+                else False
+
+-- Funcao lamba que soma dois numeros
+somaPar :: Int -> Int -> Int
+somaPar = \p q -> p + q
+
+-- Funcao que calcula o dobro de um numero
+dobro :: Int -> Int
+dobro = \y -> y * 2
