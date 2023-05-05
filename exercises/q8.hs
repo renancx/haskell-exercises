1e74de9d9767bfef86df7361d0378ac1761ed059
Renan <renan.lowe@hotmail.com>
2023-05-04 22:44:20 -0300

    exercise 8

---
+++ exercises/q8.hs
@@ -0,0 +1,5 @@
+--Funcao que seleciona todos os elementos de uma lista vazia, exceto o ultimo
+primeiros :: [Int] -> [Int]
+primeiros [] = error "Lista vazia"
+primeiros [x] = []
+primeiros (x:xs) = x : primeiros xs 
