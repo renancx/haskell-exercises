2fdda856f3079f4f2c1f9a9f1af6b8090d90f9f3
Renan <renan.lowe@hotmail.com>
2023-05-14 21:46:06 -0300

    3.25

--- exercises/q15.hs
+++ exercises/q15.hs
@@ -1,3 +1,18 @@
 countList :: [a] -> Int
 countList [] = 0
 countList l = foldl (\x y -> x + 1) 0 l
+
+-- filter, fold e map
+-- usado para manipular lista
+
+primeiro :: [Integer] -> Integer
+primeiro f = head f
+
+ultimo :: [Integer] -> Integer
+ultimo u = last u
+
+list = [1, 5, 3, 2, 4]
+
+pares :: [Integer] -> [Integer]
+pares [] = []
+pares b = (filter (not . odd) b)
