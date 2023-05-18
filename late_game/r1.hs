95aaa63de5d7e574b510cadc01c51d402213370a
Renan <renan.lowe@hotmail.com>
2023-05-18 09:11:22 -0300

    r1 update

--- late_game/r1.hs
+++ late_game/r1.hs
@@ -9,8 +9,8 @@ type Venda = (Id, [Item], FormaPagamento)
 
 exemplo :: Venda 
 exemplo = (1, 
-            [ ("Bala", 0.5, 10), 
-              ("Pirulito", 0.25, 5)
+            [ ("Bala", 1, 10), 
+              ("Pirulito", 2, 5)
             ], 
             Dinheiro
         )
@@ -25,3 +25,14 @@ totalItens (_, itens, _) = sum (map (\(_, _, qtd) -> qtd) itens)
 totalVenda :: Venda -> Double
 totalVenda (_, itens, _) = sum (map (\(_, preco, qtd) -> preco * fromIntegral qtd) itens)
 
+-- Funcao que retorna apenas os itens cujo valor unitario é maior que 1 real
+maioresQueUmReal :: Item -> Bool
+maioresQueUmReal (_, preco, _) = case preco of 
+                                    preco | preco > 1 -> True
+                                    _ -> False
+
+itensCaros :: Venda -> [Item]
+itensCaros (_, itens, _) = filter maioresQueUmReal itens
+
+total :: Venda -> [(Produto, Double)]
+total (_, itens, _) = map (\(prod, preco, qtd) -> (prod, preco * fromIntegral qtd)) itens
