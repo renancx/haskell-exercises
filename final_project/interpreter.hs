1877667838656da85fc88a69e5fcc2ff21dadbd8
Renan <renan.lowe@hotmail.com>
2023-05-25 14:24:28 -0300

    update code

--- final_project/interpreter.hs
+++ final_project/interpreter.hs
@@ -5,6 +5,7 @@ step :: Expr -> Expr
 step (Add (Num n1) (Num n2)) = Num (n1 + n2)
 step (Add (Num n) e2) = Add (Num n) (step e2)
 step (Add e1 e2) = Add (step e1) e2
+
 step (And BFalse e2) = BFalse
 step (And BTrue e2) = e2
 step (And e1 e2) = And (step e1) e2
