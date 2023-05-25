1c2db1ca17488bbdd00d29dd24529a1ebd43a524
Renan <renan.lowe@hotmail.com>
2023-05-25 15:41:17 -0300

    logic operators implementation (or & xor)

--- final_project/typeChecker.hs
+++ final_project/typeChecker.hs
@@ -15,14 +15,11 @@ typeof (And e1 e2) = case (typeof e1, typeof e2) of
     _ -> Nothing
 
 typeof (If e1 e2 e3) =
-    case typeof e1 of
+    case typeof e1 of 
         (Just TBool) -> case (typeof e1, typeof e2) of
-            (Just t1, Just t2) -> if t1 == t2 then 
-                                    Just t1 
-                                  else 
-                                    Nothing
-                                _ -> Nothing
-                  _ -> Nothing
+            (Just t1, Just t2) -> if t1 == t2 then Just t1 else Nothing
+            _ -> Nothing
+        _ -> Nothing
 
 -- typeof para operação aritmética de subtração
 typeof (Sub e1 e2) = case (typeof e1, typeof e2) of
@@ -32,4 +29,14 @@ typeof (Sub e1 e2) = case (typeof e1, typeof e2) of
 -- typeof para operação aritmética de multiplicação
 typeof (Mul e1 e2) = case (typeof e1, typeof e2) of
     (Just TNum, Just TNum) -> Just TNum
+    _ -> Nothing
+
+-- typeof para operação lógica de or
+typeof (Or e1 e2) = case (typeof e1, typeof e2) of
+    (Just TBool, Just TBool) -> Just TBool
+    _ -> Nothing
+
+-- typeof para operação lógica de xor
+typeof (Xor e1 e2) = case (typeof e1, typeof e2) of
+    (Just TBool, Just TBool) -> Just TBool
     _ -> Nothing
