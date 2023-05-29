eff51016624f29859627a8fd42c1e2b383edb555
Renan <renan.lowe@hotmail.com>
2023-05-29 17:30:03 -0300

    lexer implementation

--- final_project/lexer.hs
+++ final_project/lexer.hs
@@ -1,4 +1,5 @@
 module Lexer where 
+import Data.Char
 
 data Expr = BTrue
           | BFalse
@@ -19,3 +20,52 @@ data Expr = BTrue
 data Ty = TBool
         | TNum
         deriving (Show, Eq)
+
+data Token = TokenTrue 
+           | TokenFalse 
+           | TokenNum Int 
+           | TokenAdd 
+           | TokenSub 
+           | TokenMul 
+           | TokenAnd 
+           | TokenIf 
+           | TokenThen 
+           | TokenElse 
+           | TokenOr 
+           | TokenXor 
+           deriving (Show, Eq)
+
+isToken :: Char -> Bool
+isToken c = elem c "+-*/&|"
+
+lexer :: String -> [Token]
+lexer [] = []
+lexer (c:cs) 
+    | isSpace c = lexer cs
+    | isDigit c = lexNum (c:cs)
+    | isAlpha c = lexKW (c:cs)
+    | isToken c = lexSymbol (c:cs)
+    | otherwise = error ("Lexical error: invalid character " ++ [c])
+
+lexNum :: String -> [Token]
+lexNum cs = case span isDigit cs of
+    (num, rest) -> TokenNum (read num) : lexer rest
+
+lexKW :: String -> [Token]
+lexKW cs = case span isAlpha cs of
+                ("true", rest) -> TokenTrue : lexer rest
+                ("false", rest) -> TokenFalse : lexer rest
+                ("if", rest) -> TokenIf : lexer rest
+                ("then", rest) -> TokenThen : lexer rest
+                ("else", rest) -> TokenElse : lexer rest
+                _ -> error "Lexical error: invalid keyword"
+
+lexSymbol :: String -> [Token]
+lexSymbol cs = case span isToken cs of
+                ("+", rest) -> TokenAdd : lexer rest
+                ("-", rest) -> TokenSub : lexer rest
+                ("*", rest) -> TokenMul : lexer rest
+                ("&&", rest) -> TokenAnd : lexer rest
+                ("||", rest) -> TokenOr : lexer rest
+                ("^", rest) -> TokenXor : lexer rest
+                _ -> error "Lexical error: invalid symbol"
