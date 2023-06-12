5c457b92f61d904466d1994139954ab112812098
Renan <renan.lowe@hotmail.com>
2023-06-12 17:54:12 -0300

    typeof ctx

--- final_project/typeChecker.hs
+++ final_project/typeChecker.hs
@@ -4,42 +4,44 @@ import Parser
 
 type Ctx = [(String, Ty)]
 
-typeof :: Ctx -> Expr -> Maybe Ty
+typeof ctx :: Ctx -> Expr -> Maybe Ty
 typeof ctx BTrue = Just TBool
 typeof ctx BFalse = Just TBool
 typeof ctx (Num _) = Just TNum
 
-typeof ctx (Add e1 e2) = case (typeof e1, typeof e2) of
+typeof ctx (Add e1 e2) = case (typeof ctx e1, typeof ctx e2) of
     (Just TNum, Just TNum) -> Just TNum
     _ -> Nothing
 
-typeof ctx (And e1 e2) = case (typeof e1, typeof e2) of
+typeof ctx (And e1 e2) = case (typeof ctx e1, typeof ctx e2) of
     (Just TBool, Just TBool) -> Just TBool
     _ -> Nothing
 
 typeof ctx (If e1 e2 e3) =
-    case typeof e1 of 
-        (Just TBool) -> case (typeof e1, typeof e2) of
+    case typeof ctx e1 of 
+        (Just TBool) -> case (typeof ctx e1, typeof ctx e2) of
             (Just t1, Just t2) -> if t1 == t2 then Just t1 else Nothing
             _ -> Nothing
         _ -> Nothing
 
--- typeof para operação aritmética de subtração
-typeof ctx (Sub e1 e2) = case (typeof e1, typeof e2) of
+-- typeof ctx para operação aritmética de subtração
+typeof ctx (Sub e1 e2) = case (typeof ctx e1, typeof ctx e2) of
     (Just TNum, Just TNum) -> Just TNum
     _ -> Nothing
 
--- typeof para operação aritmética de multiplicação
-typeof ctx (Mul e1 e2) = case (typeof e1, typeof e2) of
+-- typeof ctx para operação aritmética de multiplicação
+typeof ctx (Mul e1 e2) = case (typeof ctx e1, typeof ctx e2) of
     (Just TNum, Just TNum) -> Just TNum
     _ -> Nothing
 
--- typeof para operação lógica de or
-typeof ctx (Or e1 e2) = case (typeof e1, typeof e2) of
+-- typeof ctx para operação lógica de or
+typeof ctx (Or e1 e2) = case (typeof ctx e1, typeof ctx e2) of
     (Just TBool, Just TBool) -> Just TBool
     _ -> Nothing
 
--- typeof para operação lógica de xor
-typeof ctx (Xor e1 e2) = case (typeof e1, typeof e2) of
+-- typeof ctx para operação lógica de xor
+typeof ctx (Xor e1 e2) = case (typeof ctx e1, typeof ctx e2) of
     (Just TBool, Just TBool) -> Just TBool
     _ -> Nothing
+
+typeof ctx (Var x) = Lookup x ctx
